19bfdc8d697fe7bdb82967207c482e89f6ac445c
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2017-07-27 13:52:42 -0300

    atualizado compose, compose-manager

--- docker/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ -S ${DOCKER_SOCKET} ]; then
-    DOCKER_GID=$(stat -c '%g' ${DOCKER_SOCKET})
-    groupadd -for -g ${DOCKER_GID} ${DOCKER_GROUP}
-    usermod -aG ${DOCKER_GROUP} ${JENKINS_USER}
-fi

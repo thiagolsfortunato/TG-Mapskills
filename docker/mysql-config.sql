9b876d834cafb41f9ffded35fccac02354fb749f
Thiago Fortunato <thiagolsfortunato@hotmail.com>
2017-07-29 13:44:43 -0300

    versao final docker-compose, Dockerfile jenkins, containers.txt, jenkins e mysql config

--- docker/mysql-config.sql
+++ docker/mysql-config.sql
@@ -1,3 +1,10 @@
+# Comando para acessar container docker
+docker exec -it mapskills-mysql mysql -u root -p
+senha: mapskills
+
+#permissão do usuario mapskills para acesso remoto
 GRANT ALL PRIVILEGES ON mapskills.* TO 'mapskills'@'%' IDENTIFIED BY 'mapskills' WITH GRANT OPTION;
 GRANT ALL PRIVILEGES ON mapskills.* TO 'mapskills'@'localhost' IDENTIFIED BY 'mapskills' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
+
+exit

b1ab5fd07a1fa67c5175023c994d5d6e90cea14d
Thiago Fortunato <thiago.fortunato@fotosensores.com>
2017-09-22 09:04:53 -0300

    backup ambiente producao

---
+++ bakup/mysql/mysql-conf.sql
@@ -0,0 +1,3 @@
+GRANT ALL PRIVILEGES ON mapskills.* TO 'mapskills'@'%' IDENTIFIED BY 'mapskills' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON mapskills.* TO 'mapskills'@'localhost' IDENTIFIED BY 'mapskills' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
